1becad84d97063dbe640348026360238fb858cb1
Erik <erik.alves253@gmail.com>
2021-06-21 18:18:31 -0700

    ESR - 02.07 - Criando um controller com Spring MVC

---
+++ src/main/java/com/erik/projeto/MeuPrimeiroController.java
@@ -0,0 +1,16 @@
+package com.erik.projeto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MeuPrimeiroController {
+
+	public MeuPrimeiroController() {
+	}
+	
+	@GetMapping("/hello")
+	public String hello(){
+		
+		return "Hello Erik";
+	}
+}
