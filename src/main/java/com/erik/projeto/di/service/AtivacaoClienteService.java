015f3368d89af6280aeec5d7f2372a8faa73dd54
Erik <erik.alves253@gmail.com>
2021-06-23 02:35:08 -0700

    ESR - 02.14 - Conhecendo os pontos de injeção e a anotação @Autowired

--- src/main/java/com/erik/projeto/di/service/AtivacaoClienteService.java
+++ src/main/java/com/erik/projeto/di/service/AtivacaoClienteService.java
@@ -1,29 +1,29 @@
 package com.erik.projeto.di.service;
+import org.springframework.stereotype.Component;
 import com.erik.projeto.di.modelo.Cliente;
 import com.erik.projeto.di.notificacao.Notificador;
 
 
+@Component
 public class AtivacaoClienteService {
 
-
+	//@Autowired
 	private Notificador notificador;
 	
-	
-	//-------------------------------------------------------------//
-	
-	public AtivacaoClienteService(Notificador notificador) {
-		
-		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService: " + notificador);
-	}
 
+ 	public AtivacaoClienteService(Notificador notificador) {
+		this.notificador = notificador;
+	}
 	
+
+ 	//-----------------------------------------------//
+ 	
 	public void ativar(Cliente cliente) {
-		
 		cliente.ativar();
 
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
+
+
 	
 }
